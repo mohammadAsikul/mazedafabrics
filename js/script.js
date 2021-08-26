9f92e9dfb0fa003cadf0cd4f69fabe186aba7b70
Md. Ashikul Islam <asikul.bhuiyan5027@gmail.com>
2021-08-27 03:27:45 +0600

    header section almost complated

--- js/script.js
+++ js/script.js
@@ -9,5 +9,20 @@ $(document).ready(function() {
       autoplaySpeed:1000,
       smartSpeed:1500,
       autoplayHoverPause:true,
+      // animateOut: 'slideOutDown',
+      // animateIn: 'pulse'
   });
+  // typing animation
+  var typed = new Typed(".typing", {
+    strings: ["Mazeda Fabrics Ltd", "100% Export Orianted", "Your Trasted Partner"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+  var typed = new Typed(".typing2", {
+    strings: ["Contact Us" , "Have any questions?"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  })
 })
